acbbc5902805c37e5aca843a62303807453f4a87
elarbi <ext.msahel.elarbi@reseau.sncf.fr>
2023-10-16 16:08:56 +0200

    add treemacs config

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-	 '(projectile vscode-icon vue-mode ivy which-key web-mode php-mode org-bullets doom-themes doom-modeline auto-complete)))
+	 '(treemacs-magit treemacs all-the-icons projectile vscode-icon vue-mode ivy which-key web-mode php-mode org-bullets doom-themes doom-modeline auto-complete)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
